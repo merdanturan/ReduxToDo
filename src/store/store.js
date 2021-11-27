6605efebf61656d837b1f1f432de4e391469d68f
Merdan TURAN <merdanturan@gmail.com>
2021-11-27 19:21:43 +0300

    Add and configure redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
 import todoReducer from './todoSlice'
 
-export default configureStore ({
-    reducer: {
-        todos: todoReducer
-    },
-})
+// export default configureStore ({
+//     reducer: {
+//         todos: todoReducer
+//     },
+// })
+
+const reducers = combineReducers({
+    todos: todoReducer,
+});
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = configureStore({
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: [thunk],
+});
+export default store;
