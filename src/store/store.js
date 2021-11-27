ae845104c56066e9f44ab99d07601eac0a8601df
Merdan TURAN <merdanturan@gmail.com>
2021-11-27 20:59:48 +0300

    Remove comment line

--- src/store/store.js
+++ src/store/store.js
@@ -5,23 +5,22 @@ import { persistReducer } from 'redux-persist';
 import thunk from 'redux-thunk';
 import todoReducer from './todoSlice'
 
-// export default configureStore ({
-//     reducer: {
-//         todos: todoReducer
-//     },
-// })
 
 const reducers = combineReducers({
     todos: todoReducer,
 });
+
 const persistConfig = {
     key: 'root',
     storage,
 };
+
 const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
     reducer: persistedReducer,
     devTools: process.env.NODE_ENV !== 'production',
     middleware: [thunk],
 });
+
 export default store;
